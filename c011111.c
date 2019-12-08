abdfc4d86cefd115c3e666f720a2b0d0f4717b3d
AnnPush <32917082+AnnPush@users.noreply.github.com>
2019-12-08 23:31:48 +0200

    Update c011111.c

--- c011111.c
+++ c011111.c
@@ -1,1 +1,10 @@
+//1_welcomeC
+//fig02_01.c
+//A first program in C
+#include <stdio.h>
 
+//function main begin program execution
+int main(void)
+{
+	printf("Welcome to C!\n");
+}//end function main
