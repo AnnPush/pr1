9a4f98149b0ab4ddd94135f2c71b89f298608939
AnnPush <32917082+AnnPush@users.noreply.github.com>
2019-12-16 20:58:47 +0200

    Update c20_tyuu.c

--- c20_tyuu.c
+++ c20_tyuu.c
@@ -3,5 +3,5 @@
 //kl   kljkljklgdfgdhgdhfghghghghg
 hlgglguyudfdfdfhjljh;ggnhjjlklfgfhgfjhgjgffhfgh
 iololol,j,hfghgfhfghk;l;fhjkh;hgfjhdjdghjghj
-hgljglxcvxfgdfghdf
+hgljglxcvxfgdfghdfkh;klhkl
 fdgfg
