20181b81c103a6cc185a5734828d1e074daca552
AnnPush <32917082+AnnPush@users.noreply.github.com>
2019-12-29 22:40:41 +0200

    Update c21_ghj.c

--- c21_ghj.c
+++ c21_ghj.c
@@ -3,6 +3,6 @@
 #define SALES_SIZE 20     //определение размера массива валовых продаж
 
 //hglylugjgltyrtutyjh;kl;khjfdjdjdfghfghfghgfhbvn
-//fvgnkml,hfghjcgdfgrynbvjhgjgjsdfsdfsfsrehhjgjgjgjhgj
+//fvgnkml,hfghjcgdfgrynbvjhgjgjsdfsdfsfsrehhjgjgjgjhgjhgg
 njkkjbnbvnhfjjjgfj
 bnmbnmb
