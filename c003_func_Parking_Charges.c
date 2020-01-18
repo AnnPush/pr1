38fff6256f4ebc8ac657efcf6f27174262528a4e
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-01-18 20:47:30 +0200

    Update c003_func_Parking_Charges.c

--- c003_func_Parking_Charges.c
+++ c003_func_Parking_Charges.c
@@ -1,20 +1,49 @@
-// Fig. 5.6: fig05_06.c
-// Demonstrating the function call stack
-// and stack frames using a function square.
+// c003_func_Parking_Charges.c
 #include <stdio.h>
 
-int square(int); // prototype for function square
+float calculateCharges(float hours);
 
-int main()
+int main(void)
 {
-	int a = 10; //value to square (local automatic variable in main)
+    float t1;
+	float t2;
+	float t3;
+	float ch;
+	float t;
+	
+   
+     printf("How many hours did each car stay?\n Enter time1, time2, time3:\n");
+     scanf("%f%f%f", &t1, &t2, &t3);
 
-	printf("%d squared: %d\n", a, square(a)); // display a squared
+	 printf("%s%13s%13s\n", "Car", "Hours", "Charge");
+	 printf("1.%13.1f%13.2f\n", t1, calculateCharges(t1));
+	 printf("2.%13.1f%13.2f\n", t2, calculateCharges(t2));
+	 printf("3.%13.1f%13.2f\n", t3, calculateCharges(t3));
+
+	 t = t1 + t2 + t3;
+	 ch = calculateCharges(t1) + calculateCharges(t2) + calculateCharges(t3);
+
+
+     printf("TOTAL%10.1f%13.2f\n", t, ch);
 }
-
-//returns the square of an integer
-int square(int x) // x is a local variable
+float calculateCharges(float hours)
 {
-	return x * x; // calculate square and return result
+    
+    
+    float charge;
+    
+    if(hours >= 24)
+    {
+        charge = 10.0;
+    }
+    else if(hours <= 3)
+    {
+        charge = 2.0;
+    }
+    else
+    {
+        charge = 2.0 + 0.5 *(hours - 3);
+    }
+    return charge;
 }
 
