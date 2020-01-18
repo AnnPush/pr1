16dde0d1329155cfccb9a313a3c743dee3f58750
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-01-19 00:36:57 +0200

    Update c008_func_Multiples.c

--- c008_func_Multiples.c
+++ c008_func_Multiples.c
@@ -1,63 +1,52 @@
+/*(Multiples) Write a function isMultiple that determines for a pair of integers whether the
+second integer is a multiple of the first. The function should take two integer arguments and return
+1 (true) if the second is a multiple of the first, and 0 (false) otherwise. Use this function in a program
+that inputs a series of pairs of integers.*/
 
-// Fig. 5.16: fig05_16.c
-// Scoping.
 #include <stdio.h>
 
-void useLocal(void); 
-void useStaticLocal(void); 
-void useGlobal(void);
+int isMultiple(int, int);
 
-int x = 1; // global variable
-
-int main(void)
+int main()
 {
-	int x = 5; // local variable main
+	int integer1, integer2;
 
-	printf("local x in outer score of main is %d\n", x);
-
-	{ // start new score
-		int x = 7; // local variable to new score
-
-		printf("local x in inner score of main is %d\n", x);
-	} // end new scope
-
-	printf("local x in outer score of main is %d\n", x);
-
-	useLocal(); //useLocal has automatic local x
-	useStaticLocal(); // useStaticLocal has static local x
-	useGlobal(); // useGlobal uses global x
-	useLocal(); //useLocal reinitializes automatic local x
-	useStaticLocal(); // static local x retains its prior value
-
-	printf("\nlocal x in main is %d\n", x);
+	puts("Enter integer1(enter - 0)");
+	scanf("%d", &integer1);
+	
+	while(integer1 != 0)
+	{
+		puts("Enter integer2");
+		scanf("%d", &integer2);
+		
+		if(isMultiple(integer1, integer2) == 1)
+		{
+			puts("Integer2 is a multiple of the integer1");
+		}
+		else
+		{
+			puts("Integer2 is not a multiple of the integer1");
+		}
+		
+		puts("\nEnter integer1(enter - 0)");
+		scanf("%d", &integer1);
+	}
 }
 
-// useLocal reinitializes local variable x during each call
-void useLocal(void)
+int isMultiple(int i1, int i2)
 {
-	int x = 25; // initializes each time useLocal is called
+	int b;
+	float z;
 
-	printf("\nlocal x is useLocal is %d after entering useLocal\n", x);
-	++x;
-	printf("local x in useLocal is %d before exiting useLocal\n", x);
-}
+	z = (i2 % i1);
 
-// useStaticLocal initializes static local variable x only the first time
-// the function is called; value of x is saved between calls to this function
-void useStaticLocal(void)
-{
-	// initialized once
-	static int x = 50;
-
-	printf("\nlocal static x is %d on enterrng useStaticLocal\n", x);
-		++x;
-	printf("local static x is %d on exiting useStatic Local\n", x);
-}
-
-// function useGlobal modifies global variable x during each call
-void useGlobal(void)
-{
-	printf("\nglobal x is %d on entering useGlobal\n", x);
-	x *= 10;
-	printf("global x is %d on exiting useGlobal\n", x);
+	if( z == 0)
+	{
+		b = 1;
+	}
+	else
+	{
+		b = 0;
+	}
+	return b;
 }
