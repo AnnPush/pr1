7f58eb9edded2a989290aa394a6b0088daa0d8f7
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-01-20 02:02:42 +0200

    Update c014_func_Time_in_Seconds.c

--- c014_func_Time_in_Seconds.c
+++ c014_func_Time_in_Seconds.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int isTime(int, int, int);
+
+int main()
+{
+	int hours, minutes, seconds;
+
+	printf("%s\n", "Enter three integer arguments (for hours, minutes, and seconds):");
+	scanf("%d%d%d", &hours, &minutes, &seconds);
+
+	printf("The number of seconds since the last time the clock struck 12:%d\n", isTime(hours, minutes, seconds));
+}
+
+int isTime(int h, int m, int s)
+{
+	h = h * 3600;
+	m = m * 60;
+	s = s + h + m;
+	return s;
+}
