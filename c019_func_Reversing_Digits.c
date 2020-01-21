38c5b848727762449dc48d279ba0e1700f9a6fba
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-01-21 03:20:40 +0200

    Update c019_func_Reversing_Digits.c

--- c019_func_Reversing_Digits.c
+++ c019_func_Reversing_Digits.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int isReversingDigits(int);
+
+int main()
+{
+	int digit;
+
+	printf("%s\n", "Enter digit:");
+	scanf("%d", &digit);
+
+	printf("New digit: %d\n", isReversingDigits(digit));
+}
+
+int isReversingDigits(int d)
+{
+	int newDigit =0;
+
+	while( d!= 0)
+	{
+		newDigit = (newDigit * 10) + (d % 10);
+		d = (d - (d % 10)) / 10;
+	}
+    return newDigit;
+}
