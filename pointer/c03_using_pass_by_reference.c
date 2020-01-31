8bd4ae7d9a1bc6d06dd1bab5afc55b295cc5999d
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-01-31 13:05:02 +0200

    Update and rename c22_fghjc to pointer/c03_using_pass_by_reference.c

---
+++ pointer/c03_using_pass_by_reference.c
@@ -0,0 +1,23 @@
+// Cube a variable using pass-by-reference with a pointer argument
+
+#include <stdio.h>
+
+void cubeByReference(int *nPtr);
+
+int main(void)
+{
+	int number = 5; // initialize number
+
+	printf("The original value of number is %d", number);
+
+	// pass address of number to cubeByReference
+	cubeByReference(&number);
+
+	printf("\nThe new value of number is %d\n", number);
+}
+
+// calculate cube of *nPtr; actually modifies number in main
+void cubeByReference(int *nPtr)
+{
+	*nPtr = *nPtr * *nPtr * *nPtr; // cube *nPtr
+}
