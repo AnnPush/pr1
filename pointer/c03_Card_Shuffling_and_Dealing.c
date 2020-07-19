29fec426a2dd9338b518a720e0f0852367f7ec75
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-07-19 23:23:02 +0300

    Update c03_Card_Shuffling_and_Dealing.c

--- pointer/c03_Card_Shuffling_and_Dealing.c
+++ pointer/c03_Card_Shuffling_and_Dealing.c
@@ -8,85 +8,18 @@
 
 //ïðîòîòèïû
 void shuffle(unsigned int wDeck[][FACES]); // òàñîâàòü êîëîäó êàðò
-int rating(unsigned int wPlayer[][2], const char *wFace[], const char *wSuit[], unsigned int *nominal);//îöåíêà êàðò
-int printRating(unsigned int*, unsigned int*);//ïå÷àòü ðåçóëüòàòà îöåíèâàíèÿ êàðò
 void deal(unsigned int wDeck[][FACES], unsigned int wPlayer[][2],  const char *wFace[], const char *wSuit[], unsigned int *n1,  unsigned int *f1); // ðàçäàòü êàðòû, íå èçìåíÿÿ ìàññèâ
-int combination(unsigned int wPlayer[][2], const char *wFace[], const char *wSuit[], unsigned int *nominal, unsigned int *kk1, unsigned int *kk2);
-void printArray(unsigned int wPlayer[][2], const char *wFace[], const char *wSuit[], unsigned int pd);
-void printCombination(unsigned int tt);
-void takeCards(unsigned int wPlayer[][2], unsigned int *nominal, unsigned int tt, unsigned int *kk1, unsigned int *kk2, unsigned int *f1);
+int combination(unsigned int wPlayer[][2], const char *wFace[], const char *wSuit[], unsigned int *nominal, unsigned int *kk1, unsigned int *kk2);//îïðåäåëèòü êîìáèíàöèþ êàðò
+void printArray(unsigned int wPlayer[][2], const char *wFace[], const char *wSuit[], unsigned int pd);//ðàñïå÷àòàòü êàðòû èãðîêà
+void printCombination(unsigned int tt);//ðàñïå÷àòàòü êîìáèíàöèþ
+void takeCards(unsigned int wPlayer[][2], unsigned int *nominal, unsigned int tt, unsigned int *kk1, unsigned int *kk2, unsigned int *f1);//îïðåäåëèòü, ñêîëüêî êàðò âçÿòü åùå
 
 
 int main(void)
 {
 	 //1_ èíèöèàëèçàöèÿ êîëîäû êàðò íóëÿìè(ïðè òåñòèðîâàíèè çàêîììåíòèðîâàòü ýòó ñòðîêó è âûáðàòü îäíó èç ïîñëåäóþùèõ)
-	 //unsigned int deck[SUITS][FACES] = {0};
+	  unsigned int deck[SUITS][FACES] = {0};
 
-	 //2_ èíèöèàëèçàöèÿ êîëîäû êàðò_íåò êîìáèíàöèé_ó_èãðîêîâ
-     unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21,  5, 17, 19, 11, 20, 26, 37,  9, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10,  3, 27, 16, 18, 23, 25, 34, 35, 36,  6, 38, 39, 40, 41, 14, 42, 43, 44, 45, 46,  4, 48, 49,  7, 51, 52};
-     //ìàññèâ_ïî_ïîðÿäêó
-	 //unsigned int deck[SUITS][FACES] = { 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
-	 
-	 //3_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_1_ÏÀÐÀ_ó_ïåðâîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21,  5, 17, 19, 11, 20, 26, 37,  9, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10, 27,  3, 16, 18, 23, 25, 34, 35, 36,  6, 38, 39, 40, 41, 14, 42, 43, 44, 45, 46,  4, 48, 49,  7, 51, 52};
-     
-	 //4_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_2_ÏÀÐÛ_ó_ïåðâîãî_èãðîêà
-	 //unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21,  5, 17, 19, 11, 20, 26, 37,  9, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10, 27,  3, 16, 18, 23, 25, 34, 35, 36,  6, 38, 39, 40, 41, 14, 42, 43, 44, 45, 46, 49, 48,  4,  7, 51, 52};
-     
-	 //5_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_3_ÊÀÐÒÛ_ó_ïåðâîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21,  5, 17, 19, 11, 20, 26, 37,  9, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10, 27,  3, 16, 18, 23, 25, 34, 35, 36,  6, 38, 39, 40, 41,  4, 42, 43, 44, 45, 46, 49, 48, 14,  7, 51, 52};
-    
-     //6_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_4_ÊÀÐÒÛ_ó_ïåðâîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21, 37, 17, 19, 11, 20, 26,  5,  9, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10, 27,  3, 16, 18, 23, 25, 34, 35, 36,  6, 38, 39, 40, 41,  4, 42, 43, 44, 45, 46, 49, 48, 14,  7, 51, 52};
-     
-     //7_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_ÔËÅØ_ó_ïåðâîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = { 4,  1, 13, 31,  2, 33,  3, 21,  5, 17, 19, 11, 20, 26, 37,  9, 50, 12, 30, 22, 24, 47, 32, 29, 15, 10, 27,  8, 16, 18, 23, 25, 34, 35, 36,  6, 38, 39, 40, 41, 28, 42, 43, 44, 45, 46, 49, 48, 14,  7, 51, 52};
-     
-	 //8_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_ÑÒÐÈÒ_ó_ïåðâîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = {25,  1, 13, 31,  5, 33, 50, 21, 42, 17, 19, 11, 20, 26, 37,  9,  3, 12, 30, 22, 24, 47, 32, 29, 15, 10, 27,  8, 16, 18, 23,  4, 34, 35, 36,  6, 38, 39, 40, 41, 28,  2, 43, 44, 45, 46, 49, 48, 14,  7, 51, 52};
-     
-     //9_ èíèöèàëèçàöèÿ êîëîäû êàðò_ÔËÅØ_ÄÐÎ_ó_ïåðâîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = { 4,  1, 13, 31, 48, 33,  3, 21,  5, 17, 19, 11, 20, 26, 37,  9, 50, 12, 30, 22, 24, 47, 32, 29, 15, 10, 27,  8, 16, 18, 23, 25, 34, 35, 36,  6, 38, 39, 40, 41, 28, 42, 43, 44, 45, 46, 49,  2, 14,  7, 51, 52};
-     
-	 //10_ èíèöèàëèçàöèÿ êîëîäû êàðò_ÑÒÐÈÒ_ÄÐÎ_ó_ïåðâîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = {25, 38, 13, 31,  5, 33, 50, 21, 42, 17, 19, 11, 20, 26, 37,  9,  3, 12, 30, 22, 24, 47, 32, 29, 15, 10, 27,  8, 16, 18, 23,  4, 34, 35, 36,  6,  1, 39, 40, 41, 28,  2, 43, 44, 45, 46, 49, 48, 14,  7, 51, 52};
-     
-     //11_ èíèöèàëèçàöèÿ êîëîäû êàðò_ÊÎÌÁÎ_ÄÐÎ_ó_ïåðâîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = {25,  1, 13, 31, 48, 33, 50, 21, 42, 17, 19, 11, 20, 26, 37,  9, 14, 12, 30, 22, 24, 47, 32, 29, 15, 10, 27,  8, 16, 18, 23, 46, 34, 35, 36,  6, 38, 39, 40, 41, 28, 49, 43, 44, 45,  4,  2,  5,  3,  7, 51, 52};
-     
-	 //12_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_1_ÏÀÐÀ_ó_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21,  5, 17, 19, 11, 20, 26, 37,  9, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10, 27,  3, 16, 18, 23, 25, 34, 35, 36, 40, 38, 39,  6, 41, 14, 42, 43, 44, 45, 46,  4, 48, 49,  7, 51, 52};
-    
-	 //13_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_2_ÏÀÐÛ_ó_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21,  5, 17,  9, 11, 20, 26, 37, 49, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10, 27,  3, 16, 18, 23, 25, 34, 35, 36, 40, 38, 39,  6, 41, 14, 42, 43, 44, 45, 46, 19, 48,  4,  7, 51, 52};
-    
-	 //14_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_3_ÊÀÐÒÛ_ó_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = {28,  1, 13, 31, 32, 33,  8, 21,  5, 17, 19, 11,  9, 26, 37, 20, 50, 12, 30, 22, 24, 47,  2, 29, 15, 10, 27,  3, 16, 18, 23, 25, 34, 35, 36, 40, 38, 39,  6, 41,  4, 42, 43, 44, 45, 46, 49, 48, 14,  7, 51, 52};
-    
-     //15_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_4_ÊÀÐÒÛ_ó_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = { 6,  1, 13, 31, 32, 33, 41, 21, 37, 17, 19, 11, 20,  7,  5, 28, 50, 12, 30, 22, 24, 47,  2, 29, 15, 27, 10,  3, 16, 18, 23, 25, 34, 35, 36,  9, 38, 39, 40,  8,  4, 42, 43, 44, 45, 46, 49, 48, 14, 26, 51, 52};
-     
-     //16_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_ÔËÅØ_ó_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = { 4,  1, 13, 31,  2, 33,  3, 21,  5, 17, 19, 11, 20, 26,  8, 16, 50, 12,  6, 22, 24, 10, 32,  7, 15,  9, 27, 37, 47, 18, 23, 25, 34, 35, 36, 30, 38, 39, 40, 41, 28, 42, 43, 44, 45, 46, 49, 48, 14, 29, 51, 52};
-     
-	 //17_ èíèöèàëèçàöèÿ êîëîäû êàðò_êîìáèíàöèÿ_ÑÒÐÈÒ_ó_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = {25,  1, 13, 10,  5, 33, 50, 21, 42, 17, 19, 11, 20, 26, 37,  8,  3, 12, 30, 22, 24, 47, 32, 29, 15, 31,  6, 28, 16, 18, 23,  4, 34, 35, 36, 27, 38, 39, 40, 41,  9,  2, 43,  7, 45, 46, 49, 48, 14, 44, 51, 52};
-    
-     //18_ èíèöèàëèçàöèÿ êîëîäû êàðò_ÔËÅØ_ÄÐÎ_ó_ïåðâîãî_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = { 4,  1, 13, 31, 48, 33,  3, 21,  5, 17, 19, 37, 20, 26,  8, 16, 50, 12, 30, 22, 24, 10, 32,  7, 15,  9, 27, 11, 47, 18, 23, 25, 34, 35, 36, 44, 38, 39, 40, 41, 28, 42, 43,  6, 45, 46, 49,  2, 14, 29, 51, 52};
-     
-	 //19_ èíèöèàëèçàöèÿ êîëîäû êàðò_ÑÐÈÒ_ÄÐÎ_ó_ïåðâîãî_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = {25, 38, 13, 10,  5, 33, 50, 21, 42, 17, 19, 11, 20, 26, 37,  8,  3, 12, 52, 22, 24, 47, 32, 29, 15, 31,  6, 28, 16, 18, 23,  4, 34, 35, 36, 27,  1, 39, 40, 41,  9,  2, 43, 44, 45, 46, 49, 48, 14, 30, 51,  7};
-      
-     //20_ èíèöèàëèçàöèÿ êîëîäû êàðò_ÊÎÌÁÎ_ÄÐÎ_ó_ïåðâîãî_äâóõ_èãðîêîâ
-     //unsigned int deck[SUITS][FACES] = {25,  1, 13, 31, 48, 33, 50, 21, 42, 17, 19, 11, 20, 26, 37, 47, 14, 12,  7,  6,  8,  9, 32, 29, 15, 27, 10, 24, 16, 18, 23, 46, 34, 35, 36, 22, 38, 39, 40, 41, 28, 49, 43, 44, 45,  4,  2,  5,  3, 30, 51, 52};
-     
-     //21_ èíèöèàëèçàöèÿ êîëîäû êàðò_ÔËÅØ_ó_ïåðâîãî_èãðîêà_4_ÊÀÐÒÛ_ó_âòîðîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = { 6,  1, 13, 31,  2, 33,  3, 21,  5, 17, 19, 11,  4,  7, 37, 20, 50, 12, 30, 22, 24, 47, 32, 29, 15, 27, 10, 41, 16, 18, 23, 25, 34, 35, 36,  9, 38, 39, 40,  8, 28, 42, 43, 44, 45, 46, 49, 48, 14, 26, 51, 52};
-      
-     //22_ èíèöèàëèçàöèÿ êîëîäû êàðò_ÑÒÐÈÒ_ó_ïåðâîãî_èãðîêà_3_ÊÀÐÒÛ_ó_âòîðîãî_èãðîêà
-     //unsigned int deck[SUITS][FACES] = {25,  1, 13, 31,  5, 33,  8, 21, 42, 17, 19, 11,  9, 26, 37, 20,  3, 12, 30, 22, 24, 47, 32, 29, 15, 10, 27, 50, 16, 18, 23,  4, 34, 35, 36, 40, 38, 39,  6, 41, 28,  2, 43, 44, 45, 46, 49, 48, 14,  7, 51, 52};
-      
 	 unsigned int player1[5][2]; //êàðòû íà ðóêàõ ó ïåðâîãî èãðîêà
 	 unsigned int player2[5][2]; //êàðòû íà ðóêàõ ó âòîðîãî èãðîêà
    
@@ -104,21 +37,22 @@ int main(void)
 
 	 unsigned int k1 = 100;//ïåðåìåííàÿ äëÿ çàïîìèíàíèÿ íîìèíàëà ïåðâîé ïàðû êàðò
 	 unsigned int k2 = 100;//ïåðåìåííàÿ äëÿ çàïîìèíàíèÿ íîìèíàëà âòîðîé ïàðû êàðò
-     unsigned int k3 = 0;
-	 
+     
 	 // èíèöèàëèçàöèÿ ìàññèâà ìàñòåé
      const char *suit[SUITS] = {"Hearts", "Diamonds", "Clubs", "Spades"};
 
 	 // èíèöèàëèçàöèÿ ìàññèâà íîìèíàëîâ(çíà÷åíèé)
      const char *face[FACES] = {"Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
      
-	 //òàñîâàòü êîëîäó êàðò(ïðè òåñòèðîâàíèè çàêîììåíòèðîâàòü ýòó ñòðîêó)
-     //shuffle(deck);
+	 //çàñåÿòü ãåíåðàòîð ñëó÷àéíûõ ÷èñåë 
+	 srand(time(NULL)); 
+	 //òàñîâàòü êîëîäó êàðò
+     shuffle(deck);
 
      //ñäàòü êàðòû ïåðâîìó èãðîêó
      printf("\nHand1-------------------\n\n");
      deal(deck, player1, face, suit, &start, &finish); 
-     printArray(player1, face, suit, 0); 
+     printArray(player1, face, suit, 1); 
 
      //ñäàòü êàðòû âòîðîìó èãðîêó
      printf("\nHand2-------------------\n\n");
@@ -128,18 +62,18 @@ int main(void)
 
 	 printf("\nHand1-------------------\n");
 	 combination1 = combination(player1, face, suit, &nominal1, &k1, &k2);
-	 printArray(player1, face, suit, 0);
-	 printCombination(combination1);
+	 //printArray(player1, face, suit, 0);
+	// printCombination(combination1);
 	 takeCards(player1, &nominal1, combination1, &k1, &k2, &finish);
-     printArray(player1, face, suit, 0);
+    // printArray(player1, face, suit, 0);
 	 if(finish != 0)
 	 {
 		 deal(deck, player1, face, suit, &start, &finish); 
-		 printf("\nNew_array-------------------\n");
-		 printArray(player1, face, suit, 0);
+		 //printf("\nNew_array-------------------\n");
+		 //printArray(player1, face, suit, 0);
 	 }
 	 combination1 = combination(player1, face, suit, &nominal1, &k1, &k2);
-	 printCombination(combination1);
+	// printCombination(combination1);
 
 	 printf("\nHand2-------------------\n\n");
 	 combination2 = combination(player2, face, suit, &nominal2, &k1, &k2);
@@ -147,54 +81,55 @@ int main(void)
 	 printCombination(combination2);
 }
 
-
+//îïðåäåëèòü, ñêîëüêî êàðò âçÿòü åùå
 void takeCards(unsigned int wPlayer[][2], unsigned int *nominal, unsigned int tt, unsigned int *kk1, unsigned int *kk2, unsigned int *f1)
 {
-	unsigned int wSortPlayer[5][2] = {0};
-	unsigned int x = 0;
-	unsigned int m = 0;
+	unsigned int wSortPlayer[5][2] = {0};//âðåìåííûé ìàññèâ
+	unsigned int x = 0;//ñ÷åò÷èê äëÿ êàðò, êîòîðûå áóäóò îáìåíèâàòüñÿ
+	unsigned int m = 0;//ñ÷åò÷èê êàðò, êîòîðûå ñîñòàâëÿþò êîìáèíàöèþ
 	
-    if( tt < 7 && tt > 3)
+    if( tt < 7 && tt > 3)//åñëè âûïàëî 4 îäèíàêîâûå êàðòû, ôëåø, ñòðèò
 	{
-		m = 0;
+		m = 0;//áåðåì 0 êàðò
 	}
-	else if(tt == 0 || tt == 1)
+	else if(tt == 0 || tt == 1)//åñëè íåò êîìáèíàöèè èëè âûïàëà 1 ïàðà
 	{
-		m = 3;
+		m = 3;//áåðåì 3 êàðòû
 	}
-	else if(tt == 3)
+	else if(tt == 3)//åñëè âûïàëî 3 êàðòû
 	{
-		m = 2;
+		m = 2;//áåðåì 2 êàðòû
 	}
-	else
+	else//â îñòàëüíûõ ñëó÷àÿõ
 	{
-		m = 1;
+		m = 1;//áåðåì 1 êàðòó
 	} 
-	*f1 = m;
+	*f1 = m;//çàïîìèíàåì êîëè÷åñòâî êàðò, êîòîðûå áóäóò âçÿòû
     printf("\nTake %u cards!\n\n", m);
 
-	if(tt > 7 && wPlayer[0][1] + 1 != wPlayer[1][1])
+	if(tt > 7 && wPlayer[0][1] + 1 != wPlayer[1][1])//åñëè ñòðèò äðî, êîìáî äðî è ïåðâàÿ êàðòà íå âõîäèò â êîìáèíàöèþ, òî íè÷åãî íå ìåíÿåì
 	{
 		puts("");
 	}
-	else if(tt > 6 || tt < 4)
+	else if(tt > 6 || tt < 4)//âñå âàðèàíòû, êðîìå ôëåø, 4 êàðòû, ñòðèò
     {
        for( size_t i = 0; i < 5; ++i)
 	   {
-		     if(tt > 7 && i == 0 && wPlayer[0][1] + 1 == wPlayer[1][1])
+		     if(tt > 7 && i == 0 && wPlayer[0][1] + 1 == wPlayer[1][1])//åñëè ñòðèò äðî, êîìáî äðî è ïåðâàÿ êàðòà  âõîäèò â êîìáèíàöèþ, òî ïîñëåäíÿÿ êàðòà ñòàíîâèòñÿ ïåðâîé
 	        {
 			    wSortPlayer[0][1] = wPlayer[4][1];
 			    wSortPlayer[0][0] = wPlayer[4][0];
 			}
-            else if( (tt == 7 && wPlayer[i][0] != *kk1) || (tt < 7 && wPlayer[i][1] != *kk1 && wPlayer[i][1] != *kk2) )
+            else if( (tt == 7 && wPlayer[i][0] != *kk1) || (tt < 7 && wPlayer[i][1] != *kk1 && wPlayer[i][1] != *kk2) )//åñëè ôëåø äðî è ýëåìåíò ìàññèâà ðàâåí íîìèíàëó êîìáèíàöèè èëè êîìáèíàöèÿ ìåíüøå ôëåø äðî è ýëåìåíò ìàññèâà íå ðàâåí íîìèíàëó êîìáèíàöèè
 		    {
+				//êàðòà îòïðàâëÿåòñÿ â íà÷àëî ìàññèâà
 			    wSortPlayer[x][1] = wPlayer[i][1];
 				wSortPlayer[x][0] = wPlayer[i][0];
 			    x++;
 		    }
 			else 
 		    { 
-				if(tt > 7 && m == 1 )
+				if(tt > 7 && m == 1 )//åñëè ñòðèò äðî èëè êîìáî äðî, òî êàðòó ïåðåäâèãàåì íà îäíó ïîçèöèþ âíèç
 				{
 				    i = i - 1;
 				}
@@ -203,6 +138,7 @@ void takeCards(unsigned int wPlayer[][2], unsigned int *nominal, unsigned int tt
 				++m;
 		    }
 	   }
+	   //ïåðåçàâèñàòü èç âðåìåííîãî ìàññèâà êàðòû
 	   for( size_t i = 0; i < 5; ++i)
 	   {
 	        wPlayer[i][1]=wSortPlayer[i][1];
@@ -211,8 +147,8 @@ void takeCards(unsigned int wPlayer[][2], unsigned int *nominal, unsigned int tt
 	}
 }
 
-// ïåðåòàñîâàòü êàðòû â êîëîäå(ïðè òåñòèðîâàíèè çàêîììåíòèðîâàòü ýòó ôóíêöèþ)
-/*void shuffle(unsigned int wDeck[][FACES])
+// ïåðåòàñîâàòü êàðòû â êîëîäå
+void shuffle(unsigned int wDeck[][FACES])
 {
      // äëÿ êàæäîé èç 52 êàðò ñëó÷àéíî âûáðàòü ìåñòî â êîëîäå
      for (size_t card = 1; card <= CARDS; ++card)
@@ -229,7 +165,7 @@ void takeCards(unsigned int wPlayer[][2], unsigned int *nominal, unsigned int tt
          // ïîìåñòèòü íîìåð êàðòû â âûáðàííîå ìåñòî êîëîäû
          wDeck[row][column] = card;
      }
-} */
+} 
 
 // ñäàòü êàðòû
 void deal(unsigned int wDeck[][FACES], unsigned int wPlayer[][2], const char *wFace[], const char *wSuit[],  unsigned int *n1,  unsigned int *f1)
@@ -446,18 +382,3 @@ void swap(unsigned int * element1Ptr, unsigned int * element2Ptr)
     * element2Ptr = temp;
 }
 
-//ïå÷àòü ðåçóëüòàòà îöåíèâàíèÿ êàðò
-int printRating(unsigned int* ptr1, unsigned int* ptr2)
-{
-	if(*ptr1 > *ptr2)
-	 {
-		 printf("\nHand1 is the better hand\n");
-		 return 1;
-	 }
-	 else if(*ptr1 < *ptr2)
-	 {
-         printf("\nHand2 is the better hand\n");
-		 return 1;
-	 }
-   return 0;
-}
