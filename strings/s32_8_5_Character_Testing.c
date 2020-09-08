172bb6f85d5a262022fb812e636fa13711d53a8c
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-08 21:48:37 +0300

    Update s32_8_5_Character_Testing.c

--- strings/s32_8_5_Character_Testing.c
+++ strings/s32_8_5_Character_Testing.c
@@ -4,11 +4,19 @@
 
 /*Библиотека обработки символов(<ctype.h>)
 Функции:
-int isdigit(int c);
-int isalpha(int c);
-int isalnum(int c);
-int isxdigit(int c);
-int islower(int c);
+1) int isdigit(int c);
+2) int isalpha(int c);
+3) int isalnum(int c);
+4) int isxdigit(int c);
+5) int islower(int c);
+6) int isupper(int c);
+7) int tolower(int c);
+8) int toupper(int c);
+9) int isspece(int c);
+10) int iscntrl(int c );
+11) int ispunct(int c);
+12) int isprint(int c);
+13) int isgraph(int c ).
 */
 
 #include <stdio.h>
