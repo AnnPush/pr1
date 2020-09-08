b265c48588a2479530128cf25fc3ecd511748ef6
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-08 21:18:18 +0300

    Update s32_8_5_Character_Testing.c

--- strings/s32_8_5_Character_Testing.c
+++ strings/s32_8_5_Character_Testing.c
@@ -2,6 +2,15 @@
 с помощью каждой из функций библиотеки обработки символов. Программа 
 должна выводить значения, возвращаемые каждой функцией.*/
 
+/*Библиотека обработки символов(<ctype.h>)
+Функции:
+int isdigit(int c);
+int isalpha(int c);
+int isalnum(int c);
+int isxdigit(int c);
+int islower(int c);
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 
