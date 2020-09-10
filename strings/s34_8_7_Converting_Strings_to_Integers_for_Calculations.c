fc5f78425b521a8177022ec58a2ad41fd54f7034
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-10 15:07:17 +0300

    Update s34_8_7_Converting_Strings_to_Integers_for_Calculations.c

--- strings/s34_8_7_Converting_Strings_to_Integers_for_Calculations.c
+++ strings/s34_8_7_Converting_Strings_to_Integers_for_Calculations.c
@@ -1,10 +1,24 @@
-/*Напишите программу, которая вводит 4 строки, представляющие целые значения,
- преобразует строки в целые числа, суммирует и выводит сумму 4 значений.*/
- 
- #include <stdio.h>
+#include <stdio.h>
  #include <string.h>
+ #include <stdlib.h>
+ #define SIZE 4
  
  int main(void)
- [
+ {
+	 char s[20];
+	  long x = 0;
+	
+	puts("Enter four strings that represent integers:");
+	for (int i = 0; i < SIZE; i++)
+	{
+		 scanf("%s\n", &s[i]);
+		 x = x + strtol(&s[i], 0, 0);
+		 printf("\n%ld\n", x);
+	}
+	
+	
+		 printf("\n%ld", x);
+	
  
- }
+
+}
