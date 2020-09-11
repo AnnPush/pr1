fcd461084d9c96b33f6be7f8c86fff9fbc847f34
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-11 16:28:32 +0300

    Update f13.c

--- formatting_input_output/f13.c
+++ formatting_input_output/f13.c
@@ -1,1 +1,15 @@
+/*Чтение чисел с плавающей точкой*/
 
+#include <stdio.h>
+
+int main(void)
+{
+	double a, b, c;
+	
+	puts("Enter three floating-point numbers: ");
+	scanf("%le%lf%lg", &a, &b, &c);
+	
+	printf("\nHere are the numbers entered in plain: ");
+	puts("floating-point notation:\n");
+	printf("%f\n%f\n%f\n", a, b, c);
+}
