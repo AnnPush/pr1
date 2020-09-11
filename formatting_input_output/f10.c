dccf39257f61c0f56b5bb7ba63611e0c441419e9
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-11 15:44:38 +0300

    Update f10.c

--- formatting_input_output/f10.c
+++ formatting_input_output/f10.c
@@ -1,1 +1,16 @@
+/*Использование флага # со спецификатором преобразования
+o, x, X и любыми спецификаторами с плавающей точкой*/
 
+#include <stdio.h>
+
+int main(void)
+{
+	int c = 1427;
+	printf("%#o\n", c);
+	printf("%#x\n", c);
+	printf("%#X\n", c);
+	
+	double p = 1427.0;
+	printf("\n%g\n", p);
+	printf("%#g\n", p);
+}
