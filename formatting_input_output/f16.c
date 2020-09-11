290ea1d0d6df20c5087f4ac2f3413ba55111f3a7
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-11 16:48:47 +0300

    Update f16.c

--- formatting_input_output/f16.c
+++ formatting_input_output/f16.c
@@ -1,1 +1,13 @@
+/*Использование инвертированного сканирующего набора*/
 
+#include <stdio.h>
+
+int main(void)
+{
+	char z[9];
+	
+	printf("%s", "Enter a string: ");
+	scanf("%8[^aeiou]", z);
+	
+	printf("The input was \"%s\"\n", z);
+}
