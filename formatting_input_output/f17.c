cdbb99e9cb266d31e432a797cc33e7af40159f95
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-11 18:49:11 +0300

    Update f17.c

--- formatting_input_output/f17.c
+++ formatting_input_output/f17.c
@@ -1,1 +1,12 @@
+/*Ввод данных с шириной поля*/
+#include <stdio.h>
 
+int main(void)
+{
+	int x, y;
+	
+	printf("%s", "Enter a six digit integer: ");
+	scanf("%2d%d", &x, &y);
+	
+	printf("The integers input were %d and %d\n", x, y);
+}
