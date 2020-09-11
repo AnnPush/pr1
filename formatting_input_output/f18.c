20340575091a434c6798c8a7601717ea61363dd3
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-11 18:55:52 +0300

    Update f18.c

--- formatting_input_output/f18.c
+++ formatting_input_output/f18.c
@@ -1,1 +1,18 @@
+/*Чтение и отбрасывание символов из входного потока*/
 
+#include <stdio.h>
+
+int main(void)
+{
+	int month = 0, day = 0, year = 0;
+	
+	printf("%s", "Enter a date in the form mm-dd-yyyy: ");
+	scanf("%d%*c%d%*c%d", &month, &day, &year);
+	printf("month = %d day = %d year = %d\n\n", month, day, year);
+	
+	printf("%s", "Enter a date in the form mm/dd/yyyy: ");
+	scanf("%d%*c%d%*c%d", &month, &day, &year);
+	printf("month = %d day = %d year = %d\n\n", month, day, year);
+	
+	
+}
