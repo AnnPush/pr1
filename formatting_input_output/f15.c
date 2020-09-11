25b27fbe21ac5b80cfab47669d4f94818eeff77f
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-09-11 16:43:24 +0300

    Update f15.c

--- formatting_input_output/f15.c
+++ formatting_input_output/f15.c
@@ -1,1 +1,13 @@
+/*Использование сканирующего набора*/
 
+#include <stdio.h>
+
+int main(void)
+{
+	char z[9];
+	
+	printf("%s", "Enter string: ");
+	scanf("%8[aeiou]", z);
+	
+	printf("The input was \"%s\"\n", z);
+}
