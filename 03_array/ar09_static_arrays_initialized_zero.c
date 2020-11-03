93637f14c1fb14bec1df10d9dbc4320cdb6c73aa
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-11-03 22:40:32 +0200

    Update ar09_static_arrays_initialized_zero.c

--- 03_array/ar09_static_arrays_initialized_zero.c
+++ 03_array/ar09_static_arrays_initialized_zero.c
@@ -11,7 +11,7 @@ int main(void)
 	staticArrayInit();
 	automaticArrayInit();
 
-	puts("\n\Eecond call to each function:");
+	puts("\n\nSecond call to each function:");
 
 	staticArrayInit();
 	automaticArrayInit();
